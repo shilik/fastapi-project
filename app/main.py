8e247d4b21194f514b52ac98dd149e39d6d83cc4
shilik <ltshieh@gmail.com>
2023-09-17 17:42:31 +0800

    first commit for fastapi-project

---
+++ app/main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+import uvicorn
+from os import getenv
+from app.model import Todo
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# Get all todos
+todos = []
+
+
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+
+# create a todo
+@app.post("/todos")
+async def create_todos(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo has been added"}
+
+
+# Get single todo
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "Todo not found"}
+
+
+# Delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "Todo has been deleted"}
+
+
+# Update a todo
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return {"message": "Todo has been updated", "todo": todo}
+    return {"message": "Todo not found to update"}
+
+
+if __name__ == "__main__":
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
