8e1fa2c2da6d89200d0abca35ec6235e3a3d5d9a
shilik <ltshieh@gmail.com>
2023-09-22 20:47:27 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -14,11 +14,26 @@ async def root():
         for m in palm.list_models()
         if "generateText" in m.supported_generation_methods
     ]
-    for m in models:
-        # print(f"Model Name:{m.name}")
-        return {"Model Name": m.name}
+    # for m in models:
+    # print(f"Model Name:{m.name}")
+    # return {"Model Name": m.name}
 
+    model = models[0].name
+    # return {"Model Name": model}
+
+    prompt = """
+    Summarize this paragraph and detail some relevant context.
+    Text: "I am by birth a Genevese, and my family is one of the most distinguished of that republic. My ancestors had been for many years"
+    Summary: In this text, the narrator is describing his background and upbringing. He tells us that he is a native of Geneva, Switzerland.
+    Text: "The thing the Time Traveller held in his hand was a glittering metallic framework, scarcely larger than a small clock, and very good"
+    """
     # return {"message": "Hello World From Fast API", "data": 0}
+
+    completion = palm.generate_text(
+        model=model, prompt=prompt, temperature=0.3, max_output_tokens=800
+    )
+
+    return {"completion result:": completion.result}
 
 
 @app.get("/random")
