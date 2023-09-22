290105544e66b6edbbf328f0a5a90ad96c5d9ee7
shilik <ltshieh@gmail.com>
2023-09-22 20:38:16 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 import random
 import google.generativeai as palm
 
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World From Fast API", "data": 0}
+    palm.configure(api_key="AIzaSyDIM7Ozafihl3KFtpj-35NlmqcCCDITfqM")
+    models = [
+        m
+        for m in palm.list_models()
+        if "generateText" in m.supported_generation_methods
+    ]
+    for m in models:
+        # print(f"Model Name:{m.name}")
+        return {"Model Name": m.name}
+
+    # return {"message": "Hello World From Fast API", "data": 0}
 
 
 @app.get("/random")
